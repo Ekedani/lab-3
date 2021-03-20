fdc3cb6591020ad72cfbc0d688da006056a003ce
Vladlen <ilusenkovladlen@gmail.com>
2021-03-20 21:01:20 +0200

    Дописаны функции считывания матрицы

--- library.cpp
+++ library.cpp
@@ -3,13 +3,29 @@
 char** getMatrix(string way_to_file){
     ifstream file;
     file.open(way_to_file);
+
+    //Находим размерность матрицы
     int lines = countNumberOfLines(way_to_file);
     int columns = countNumberOfColumns(way_to_file);
+
+    //Создание матрицы-лабиринта
     char **matrix = new char*[lines];
     for(int i = 0; i < lines; i++){
         matrix[i] = new char[columns];
     }
-    //TODO: написать запись в матрицу каждого второго символа кроме /n
+
+    //Заполнение матрицы, не учитывая пробелы
+    for(int i = 0; i < lines; i++){
+        for(int j = 0; j < columns * 2; j++){
+            if(j % 2 == 0){
+                matrix[lines][columns/2] = file.get();
+            }
+            else{
+                file.get();
+            }
+        }
+    }
+    file.close();
     return matrix;
 }
 
@@ -23,6 +39,7 @@ int countNumberOfLines(string way_to_file){
         getline(file, tmp);
         result += 1;
     }
+    file.close();
     return result;
 }
 
@@ -31,11 +48,21 @@ int countNumberOfColumns(string way_to_file){
     ifstream file;
     file.open(way_to_file);
     char tmp = ' ';
+
+    //Ищем количество символов в строке
     while(tmp != '\n'){
-        //TODO: переписать с использованием get
         tmp = ' ';
+        tmp = file.get();
         result += 1;
     }
-    result -= 1;
+
+    //Учитываем пробелы в строке
+    if(result % 2 == 0){
+        result /= 2;
+    }
+    else{
+        result /= 2 + 1;
+    }
+    file.close();
     return result;
 }
