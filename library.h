9eabf4e8e59c0d27c44107f0d57f46d43890e506
Vladlen <ilusenkovladlen@gmail.com>
2021-03-20 20:09:03 +0200

    Добавлена структура Cell

--- library.h
+++ library.h
@@ -5,6 +5,15 @@
 #include <string>
 #include <vector>
 
+struct Cell{
+    int i_coord;
+    int j_coord;
+    Cell(int i, int j){
+        i_coord = i;
+        j_coord = j;
+    }
+};
+
 using namespace std;
 
 
