e430b8e424d3581230beb51d5b29e7fe69cad734
Vladlen <ilusenkovladlen@gmail.com>
2021-03-21 11:27:35 +0200

    Начато написание функции по поиску пути

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,5 +1,34 @@
-#include <vector>
-#include <limits.h>
-using namespace std;
 #include "dijkstra.h"
 
+void findWay(Cell start, Cell end, char **matrix, vector<vector<int>> visited){
+    Queue<Cell> list_of_cells;
+    Cell current_cell = start;
+    do{
+        //Добавление в очередь соседних вершин
+        if(tryToAdd(current_cell.row - 1, current_cell.column, matrix, visited)){
+            list_of_cells.pushBack(Cell(current_cell.row - 1, current_cell.column, current_cell.distance + 1, &current_cell), current_cell.distance + 1);
+        }
+        if(tryToAdd(current_cell.row + 1, current_cell.column, matrix, visited)){
+            list_of_cells.pushBack(Cell(current_cell.row + 1, current_cell.column, current_cell.distance + 1, &current_cell), current_cell.distance + 1);
+        }
+        if(tryToAdd(current_cell.row, current_cell.column + 1, matrix, visited)){
+            list_of_cells.pushBack(Cell(current_cell.row, current_cell.column + 1, current_cell.distance + 1, &current_cell), current_cell.distance + 1);
+        }
+        if(tryToAdd(current_cell.row, current_cell.column - 1, matrix, visited)){
+            list_of_cells.pushBack(Cell(current_cell.row, current_cell.column - 1, current_cell.distance + 1, &current_cell), current_cell.distance + 1);
+        }
+    }
+    //TODO: перегрузить операцию сровнения для класса Cell
+    while(start != current_cell);
+
+
+}
+
+//Проверяет посещалась ли вершина ранее и проходима ли она
+bool tryToAdd(int i, int j, char** matrix, vector<vector<int>> visited){
+    bool result = false;
+    if(matrix[i][j] != 'X' && visited[i][j] != 1){
+        result = true;
+    }
+    return result;
+}
--- dijkstra.h
+++ dijkstra.h
@@ -1,4 +1,6 @@
 #include <vector>
+#include <limits.h>
+#include "Queue.h"
 using namespace std;
 
 class Cell {
@@ -6,9 +8,19 @@ public:
     int row;
     int column;
     int distance;
+    Cell *parent;
     Cell(int row_value, int column_value, int distance_value){
         row = row_value;
         column = column_value;
         distance = distance_value;
     }
+    Cell(int row_value, int column_value, int distance_value, Cell *parent_cell){
+        row = row_value;
+        column = column_value;
+        distance = distance_value;
+        parent = parent_cell;
+    }
 };
+
+void findWay(Cell start, Cell end, char **matrix, vector<vector<int>> visited);
+bool tryToAdd(int i, int j, char** matrix, vector<vector<int>> visited);
