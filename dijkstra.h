215fa026f7ab9684e4344a1a938022ace7c9749b
Vladlen <ilusenkovladlen@gmail.com>
2021-03-21 10:54:51 +0200

    Добавлен указатель на родительскую клетку в классе Cell

--- dijkstra.h
+++ dijkstra.h
@@ -6,10 +6,17 @@ public:
     int row;
     int column;
     int distance;
+    Cell *parent;
     Cell(int row_value, int column_value, int distance_value){
         row = row_value;
         column = column_value;
         distance = distance_value;
+    }
+    Cell(int row_value, int column_value, int distance_value, Cell *parent_cell){
+        row = row_value;
+        column = column_value;
+        distance = distance_value;
+        parent = parent_cell;
     }
 };
 
