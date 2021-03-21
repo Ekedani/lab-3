052a8b304bfc4a02ae984965389504f4a90bfb42
koziuberda <koziuberdaseverin@gmail.com>
2021-03-21 05:42:42 +0200

    Начато написание функции по созданию графа

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -3,3 +3,32 @@
 using namespace std;
 #include "dijkstra.h"
 
+Graph make_graph(char **matrix, int rows, int columns, vector<vector<int>> &visited){
+    int V = 0;
+    vector<pair<Cell, Cell>> edges;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            if (matrix[i][j]=' '){
+              V++;
+              visited[i][j] = 1;
+              if(i>=1 && matrix[i-1][j]==' ') {
+                  //TODO: добавить как соседа
+              }
+
+              if(i<rows-1 && matrix[i+1][j]==' '){
+                  //TODO: добавить как соседа
+              }
+
+              if(j>=1 && matrix[i][j-1]==' '){
+                  //TODO: добавить как соседа
+              }
+
+              if(j<columns-1 && matrix[i][j+1]==' '){
+                  //TODO: добавить как соседа
+              }
+
+            }
+        }
+    }
+    //не скомпилит , очевидно, сначала обработать , потом создать граф, потом его вернуть
+}
--- main.cpp
+++ main.cpp
@@ -30,4 +30,5 @@ int main() {
         visited_cells.push_back(row);
     }
 
+    Graph paths;
 }
