6454ab6e24332c43e6b216edd4622b2777947715
Vladlen <ilusenkovladlen@gmail.com>
2021-03-21 11:35:14 +0200

    Прописана примерная логика работы программы

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -3,7 +3,10 @@
 void findWay(Cell start, Cell end, char **matrix, vector<vector<int>> visited){
     Queue<Cell> list_of_cells;
     Cell current_cell = start;
-    do{
+    //TODO: перегрузить операцию сровнения для класса Cell по координатам
+    //TODO: обновить приоритетность очереди от меньшего к большему
+    while(start != current_cell){
+
         //Добавление в очередь соседних вершин
         if(tryToAdd(current_cell.row - 1, current_cell.column, matrix, visited)){
             list_of_cells.pushBack(Cell(current_cell.row - 1, current_cell.column, current_cell.distance + 1, &current_cell), current_cell.distance + 1);
@@ -17,11 +20,12 @@ void findWay(Cell start, Cell end, char **matrix, vector<vector<int>> visited){
         if(tryToAdd(current_cell.row, current_cell.column - 1, matrix, visited)){
             list_of_cells.pushBack(Cell(current_cell.row, current_cell.column - 1, current_cell.distance + 1, &current_cell), current_cell.distance + 1);
         }
+
+        //Обновление текущей вершины
+        current_cell = list_of_cells.getFront();
+        list_of_cells.popFront();
     }
-    //TODO: перегрузить операцию сровнения для класса Cell
-    while(start != current_cell);
-
-
+    //TODO: написать рекурсивную функцию, что принимает в себя Start и через parent ищет путь в начальную вершину
 }
 
 //Проверяет посещалась ли вершина ранее и проходима ли она
